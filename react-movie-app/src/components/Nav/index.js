78d6b99e024f12bc5f4f2c5d5a3010f94c57be1a
Vinh Nguyen <npcv4129@gmail.com>
2023-01-18 16:24:43 +0700

    base-project-config

--- react-movie-app/src/components/Nav/index.js
+++ react-movie-app/src/components/Nav/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <span>Home</span>
+      <span>Movies</span>
+    </div>
+  );
 }
 
 export default Nav;
